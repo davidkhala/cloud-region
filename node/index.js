79dca7256ed635ad9688f87d44a4027d9073380c
David Liu <david-khala@hotmail.com>
2025-06-30 21:19:00 +0800

    backup

--- node/index.js
+++ node/index.js
@@ -1,13 +1,5 @@
-import {Region} from "@davidkhala/gcp-compute/region.js"
 import EC2 from "@davidkhala/aws-ec2/index.js"
 
-export async function getGCP(auth) {
-    const region = new Region(auth)
-    await region.connect()
-    const list = await region.list()
-    await region.disconnect()
-    return list
-}
 
 export async function getAWS() {
     const client = new EC2()
--- node/test/gcp.js
+++ node/test/gcp.js
@@ -1,14 +1,48 @@
-import {getGCP} from '../index.js'
+import {execSync} from '@davidkhala/light/devOps.js'
+import {API, QueryBuilder} from "@davidkhala/gcp-format/region/api.js";
+import fs from 'node:fs'
+import {FromFile, ToFile} from '@davidkhala/csv/index.js'
 
-describe('gcp', function () {
+describe('location finder', function () {
     this.timeout(0)
-    const {client_email, private_key} = process.env
-    const auth = process.env.CI ? {
-        client_email, private_key, projectId: 'freetier-only'
-    } : undefined
-    it('list', async () => {
-        const result = await getGCP(auth)
-        console.debug(result)
-    })
+    const token = execSync('gcloud auth print-access-token').trim()
+    const projectId = execSync('gcloud config get-value project').trim()
+    const f = new API(token, projectId)
+    it('', async () => {
+        const file = '../regions.csv'
+        const {data, meta} = FromFile(file, false, ',')
 
+        const fields = ['Country Code','AWS ', 'AZURE', 'GCP', 'OCI']
+        for (const row of data) {
+            const code = row['Country Code']
+            let query = new QueryBuilder().condition('territory_code', '=', code)
+            const results = await f.list(query.build())
+            if (results) {
+                const aws = [], azure = [], gcp = [], oci = []
+                for (const {displayName, cloudProvider} of results) {
+                    switch (cloudProvider) {
+                        case 'CLOUD_PROVIDER_AWS':
+                            aws.push(displayName)
+                            break;
+                        case 'CLOUD_PROVIDER_GCP':
+                            gcp.push(displayName)
+                            break;
+                        case 'CLOUD_PROVIDER_AZURE':
+                            azure.push(displayName)
+                            break;
+                        case 'CLOUD_PROVIDER_OCI':
+                            oci.push(displayName)
+                            break;
+                    }
+                }
+                row['AWS'] = aws
+                row['AZURE'] = azure
+                row['GCP'] = gcp
+                row['OCI'] = oci
+            }
+
+        }
+        fs.writeFileSync(file,ToFile({data,fields}, {newline: meta.linebreak}))
+
+    })
 })
